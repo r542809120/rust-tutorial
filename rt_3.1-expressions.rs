ef8d07ba6a2f458c6ba522397eef1572f77c621f
r542809120 <r542809120@gmx.com>
2013-09-27 14:26:06 +0200

    Add tutorial part 3.1

--- rt_3.1-expressions.rs
+++ rt_3.1-expressions.rs
@@ -1,3 +1,15 @@
 fn main()
 {
+    // TODO: check why we can't use ~str or something else, the syntax is strange
+    static ITEM_SALAD: &'static str = &"salad";
+
+    let item = "salad";
+    let price =
+        if item == ITEM_SALAD {
+            12
+        } else {
+            42
+        };
+
+    println(format!("The price of the {:s} is ${:d}", item, price));
 }
